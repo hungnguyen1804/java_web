58c64dad619be1c6ee26b97d0eaab79e03cb98d8
hungnguyen1804 <nguyendinhhung1804@gmail.com>
2022-03-07 00:23:06 +0700

    first commit

---
+++ src/main/java/com/laptrinhjavaweb/dao/impl/NewDAO.java
@@ -0,0 +1,66 @@
+package com.laptrinhjavaweb.dao.impl;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+import com.laptrinhjavaweb.dao.INewDAO;
+import com.laptrinhjavaweb.mapper.NewMapper;
+import com.laptrinhjavaweb.model.NewModel;
+
+public class NewDAO extends AbstractDAO<NewModel> implements INewDAO {
+	@Override
+	public List<NewModel> findByCategoryId(Long categoryId) {
+		String sql = "SELECT * FROM news WHERE categoryid = ?";
+		return query(sql, new NewMapper(), categoryId);
+	}
+
+	@Override
+	public Long save(NewModel newModel) {
+		ResultSet resultSet = null;
+		Long id = null;
+		Connection connection = null;
+		PreparedStatement statement = null;
+		try {
+			String sql = "INSERT INTO new (title, content, categoryId) VALUES (?, ?, ?)";
+			connection = getConnection();
+			statement = connection.prepareStatement(sql);
+			statement.setString(1, newModel.getTitle());
+			statement.setString(2, newModel.getContent());
+			statement.setLong(3, newModel.getCategoryId());
+			statement.executeUpdate();
+			resultSet = statement.getGeneratedKeys();
+			if (resultSet.next()) {
+				id = resultSet.getLong(1);
+			}
+			connection.commit();
+			return id;
+		} catch (SQLException e) {
+			if (connection != null) {
+				try {
+					connection.rollback();
+				} catch (SQLException e1) {
+					e1.printStackTrace();
+				}
+			}
+			return null;
+		} finally {
+			try {
+				if (connection != null) {
+					connection.close();
+				}
+				if (statement != null) {
+					statement.close();
+				}
+				if (resultSet != null) {
+					resultSet.close();
+				}
+			} catch (SQLException e) {
+				return null;
+			}
+		}
+
+	}
+}
---
+++ src/main/java/com/laptrinhjavaweb/model/UserModel.java
@@ -0,0 +1,47 @@
+package com.laptrinhjavaweb.model;
+
+import java.sql.Timestamp;
+
+@SuppressWarnings("unused")
+public class UserModel extends AbstractModel {
+	private String userName;
+	private String fullName;
+	private String password;
+	private int status;
+	private Long roleId;
+
+	
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getFullName() {
+		return fullName;
+	}
+	public void setFullName(String fullName) {
+		this.fullName = fullName;
+	}
+	public Long getRoleId() {
+		return roleId;
+	}
+	public void setRoleId(Long roleId) {
+		this.roleId = roleId;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public int getStatus() {
+		return status;
+	}
+	public void setStatus(int status) {
+		this.status = status;
+	}
+
+	
+	
+}
